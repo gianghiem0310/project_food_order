4d6869a17235c19b61b04f2cfa7820eb545ade4b
gianghiem0310 <jangiim93nt@gmail.com>
2023-05-12 14:58:20 +0700

    1-core

--- DoAnBE2/routes/web.php
+++ DoAnBE2/routes/web.php
@@ -13,27 +13,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/home', function () {
-    return view('CustomerInterface/index');
-})->name('home');
-Route::get('shop', function () {
-    return view('CustomerInterface/shop-grid');
-})->name('shop'); 
-Route::get('detail', function () {
-    return view('CustomerInterface/shop-details');
-})->name('detail'); 
-Route::get('cart', function () {
-    return view('CustomerInterface/shoping-cart');
-})->name('cart'); 
-Route::get('contact', function () {
-    return view('CustomerInterface/contact');
-})->name('contact'); 
-Route::get('checkout', function () {
-    return view('CustomerInterface/checkout');
-})->name('checkout'); 
-Route::get('login', function () {
-    return view('LoginRegister/login');
-})->name('login'); 
-Route::get('register', function () {
-    return view('LoginRegister/register');
-})->name('register');
+Route::get('/', function () {
+    return view('welcome');
+});
